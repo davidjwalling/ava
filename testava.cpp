70283efe6f7367eb2c9eeee7aa6995924dca3719
David J. Walling <david@davidjwalling.com>
2024-05-26 20:48:41 -0500

    update

--- testava.cpp
+++ testava.cpp
@@ -1,56 +1,46 @@
-#include "api.h"
+#include "iapi.h"
 
-#include <cstdio>
-#include <cstring>
-
-namespace testava {
-    const char* all = "all";
-    const char* dump = "dump";
-    const char* help = "help";
-    const char* test = "test";
-}
+#include <iostream>
 
 bool _dump = false;
 bool _all = false;
 
 void runTests()
 {
-    printf("\n");
-    printf("Tests Completed\n");
+    std::cout << std::endl;
+    std::cout << "Tests Completed" << std::endl;
 }
 
 int usage()
 {
-    printf("Usage: testava test [all] [dump]\n");
+    std::cout << "Usage: testava [all] [dump]" << std::endl;
     return 0;
 }
 
 int __cdecl main(int argc, char* argv[])
 {
-    printf("Ava Test Program [0.X]\n");
-    printf("Copyright 2010 David J. Walling. MIT License.\n");
-    if (argc > 4 || (argc == 2 && !strcmp(argv[1], testava::help)))
+    std::cout << "Ava Test Program [0.X]" << std::endl;
+    std::cout << "Copyright 2010 David J. Walling. MIT License." << std::endl;
+
+    if (argc > 3) {
         return usage();
-    char* args[4] = { 0 };
-    for (int n = 0; n < argc; n++)
-        args[n] = argv[n];
-    if (1 == argc) {
-        char cmd[256] = { 0 };
-        fflush(stdin);
-        fgets(cmd, sizeof cmd - 1, stdin);
-        char* nexttok = cmd;
-        for (int n = 1; nexttok && n < 4; n++)
-            args[n] = tokstrx(nexttok, " \t", " \t\n", &nexttok);
     }
-    if (args[1] && !strcmp(args[1], testava::test)) {
-        for (int n = 2; n < 4 && args[n]; n++) {
-            if (!strcmp(args[n], testava::all))
-                _all = true;
-            else if (!strcmp(args[n], testava::dump))
-                _dump = true;
+    if (argc > 2) {
+        if ((std::string(argv[1]) == "all" && std::string(argv[2]) == "dump") || (std::string(argv[1]) == "dump" && std::string(argv[2]) == "all")) {
+            _all = true;
+            _dump = true;
+        } else {
+            return usage();
         }
-        runTests();
-    } else
-        return usage();
+    } else if (argc > 1) {
+        if (std::string(argv[1]) == "all") {
+            _all = true;
+        } else if (std::string(argv[1]) == "dump") {
+            _dump = true;
+        } else {
+            return usage();
+        }
+    }
+    runTests();
     return 0;
 }
